f8ddc438c235ec3f5e9e92d7c47845422e6283ef
Sai Kumar <saikumar13637@gmail.com>
2026-09-03 21:55:17 +0530

    Fix ImportError in wsgi.py for Gunicorn boot

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
-from app import app, processor
-
-# Ensure background RTSP thread processor is active when spawned by Gunicorn
-if not processor.running:
-    processor.start()
+from app import app
 
 if __name__ == "__main__":
     app.run()
